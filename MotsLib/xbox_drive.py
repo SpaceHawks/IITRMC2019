bed35752f50ac2c523edf32bd8e27e09d1c67599
Dustin Van Tate Testa <toast27@gmail.com>
2019-02-28 13:22:01 -0600

    improve readability

--- MotsLib/xbox_drive.py
+++ MotsLib/xbox_drive.py
@@ -3,13 +3,15 @@ import comms;
 import time;
 import signal;
 import sys;
+
+
+# make it so that when user ends manual control the robot is first told to stop moving
 def signal_handler(sig, frame):
     print('You pressed Ctrl+C!');
     comms.send([1, 10, 0, 0]);
     sys.exit(0);
 
 signal.signal(signal.SIGINT, signal_handler);
-#signal.pause();
 
 
 # get the joystick speed from the xbox controller
@@ -22,17 +24,20 @@ def get_speed_turn(joy):
 
 # send command to the robot
 def send_drive_cmd(speed, turn):
-    V = (100 - abs(turn)) * (speed / 100) + speed;
-    W = (100 - abs(speed)) * (turn / 100) + turn;
-    
-    comms.send([1, 11, (V+W)/2, (V-W)/2]);
+	# convert speed + turn to l & r motor speeds
+	v = (100 - abs(turn)) * (speed / 100) + speed;
+	w = (100 - abs(speed)) * (turn / 100) + turn;
+	l = (v + w) / 2;
+	r = (v - w) / 2;
+	# send tank drive command
+    comms.send([1, 11, l, r]);
+
 
 # xbox controller
 joy = xbox.Joystick();
-
 
 # main loop
 while True:
     speed, turn = get_speed_turn(joy);
     send_drive_cmd(speed, turn);
-    time.sleep(1 / 8); # 8fps
+    time.sleep(1 / 8); # 8 fps
