40b354e1c0ea7c81fafe7f8dc0bf7b6ec37ec8cc
Louis Lim <ylim@hawk.iit.edu>
2019-03-17 17:00:54 -0500

    added main with macro variables

---
+++ pathfinding.py
@@ -0,0 +1,11 @@
+import math
+import random
+
+#Main Class for Pathfinding
+class Pathfinding():
+    #Define obtacles size (radius)
+    OBJ_SIZE = 2
+
+    #Define robot size(radius) and sensor range of view
+    ROBOT_SIZE = 20
+    
