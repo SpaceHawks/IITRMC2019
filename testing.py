91d004d5cb34572a3d9231608ca06c145ef5c5a4
Louis Lim <ylim@hawk.iit.edu>
2019-03-18 18:30:37 -0500

    create testcase

--- testing.py
+++ testing.py
@@ -3,7 +3,7 @@ from pathfinding import Pathfinding
 pf = Pathfinding(0, 200)
 pf.obstacle = [[300.0, 200.0, 15.0],
                 [75.0, 150.0, 15.0]] # works with only 2 obstacles at any time
-pf.pathFinder(1)
+pf.pathFinder(0)
 a= 0.0
 
 if(a==None):
