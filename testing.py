3355a56e6b665760009ea5668d373be2fe63f5b3
Louis Lim <ylim@hawk.iit.edu>
2019-03-18 18:42:23 -0500

    Added comment for testcase

--- testing.py
+++ testing.py
@@ -3,4 +3,4 @@ from pathfinding import Pathfinding
 pf = Pathfinding(0, 200)    #starting coordinate of robot
 pf.obstacle = [[300.0, 200.0, 15.0],
                 [75.0, 150.0, 15.0]] # coordinate of next 2 obstacles, can be 0
-pf.pathFinder(0)        #arugment = number of obstacle
+pf.pathFinder(0)        #argument = number of obstacle
