8ec59e017f02ff193473ee4fa3786edb50a58704
Dustin Van Tate Testa <toast27@gmail.com>
2019-05-02 00:29:27 -0500

    1/32 too fast; reset pin different; ordering

--- MotsLib/comms.py
+++ MotsLib/comms.py
@@ -3,17 +3,18 @@ import time;
 ser = serial.Serial("/dev/ttyS1", 9600);
 
 def send(data):
-	# append checksum to end of data
-	data.append((-sum(data)) % 256);
 
 	# convert data to ints
 	data = list(map(int, data));
+
+	# append checksum to end of data
+	data.append((-sum(data)) % 256);
 
 	# verify all elems are one byte
 	data = checkData(data);
 	data = bytearray(data);
 	ser.write(data);
-	time.sleep(1 / 32);
+	time.sleep(1 / 8);
 
 # verify all elems are one byte
 def checkData(data):
@@ -34,12 +35,13 @@ import RPi.GPIO as GPIO;
 
 # reset arduino
 def reset_arduino():
+	# tinker is connected to arduino reset pin
 	GPIO.setmode(GPIO.BCM);
 	GPIO.setwarnings(False);
-	GPIO.setup(18, GPIO.OUT);
+	GPIO.setup(15, GPIO.OUT);
 	time.sleep(5);
-	GPIO.output(18, GPIO.LOW);
+	GPIO.output(15, GPIO.LOW);
 	time.sleep(.2);
-	GPIO.output(18, GPIO.HIGH);
+	GPIO.output(15, GPIO.HIGH);
 
 reset_arduino();
